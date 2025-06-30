feedf3b7c914bfe4bd603ca0cda57b3378f42165
Dereck Eder <decknho26@gmail.com>
2025-06-30 18:13:06 -0300

    Implement home view and update URL routing for tarefas app; add initial migration for Tarefa model

--- setup/urls.py
+++ setup/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from tarefas.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tarefas/', include('tarefas.urls')),  # Include the URLs from the tarefas app
+    path('', home, name='home'),  # Home view for tarefas app
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    """
-    Render the home page for the tarefas app.
-    """
     return render(request, 'tarefas/home.html')
