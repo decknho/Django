46ada9a3c3a8880670e086d3c0c1aab0abd76fbe
Dereck Eder <decknho26@gmail.com>
2025-06-30 19:48:43 -0300

    sla

--- tarefas/models.py
+++ tarefas/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=200, null=False, blank=False)
     criada_em = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    prazo = models.DateTimeField(null=False, blank=False)
-    concluida_em = models.DateTimeField(null=True)
+    prazo = models.DateField(null=False, blank=False)
+    concluida_em = models.DateField(null=True)
