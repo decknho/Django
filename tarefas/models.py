745bcd2687a752c5303336e517b9f34c7ce62e83
Dereck Eder <decknho26@gmail.com>
2025-06-30 19:04:11 -0300

    Update Tarefa model fields: set descricao and concluida_em to allow null values; add tarefas_lista view to render tasks

--- tarefas/models.py
+++ tarefas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=200, null=False, blank=False)
-    descricao = models.TextField()
+    descricao = models.TextField(null=True)
     criada_em = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     prazo = models.DateTimeField(null=False, blank=False)
-    concluida_em = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    concluida_em = models.DateTimeField(null=True)
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Tarefa
+
 
 def tarefas_lista(request):
-    return render(request, "tarefas/tarefas_lista.html")
+    tarefas = Tarefa.objects.all()
+    return render(request, "tarefas/tarefas_lista.html", {"tarefas": tarefas})
